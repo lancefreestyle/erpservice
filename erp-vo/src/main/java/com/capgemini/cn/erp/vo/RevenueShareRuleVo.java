a2388cbb9f4e6a7cf48f5aabd27cf3f8435a0236
Alon.Du <alon.du@hotmail.com>
2019-10-09 21:39:36 +0800

    fix vo

--- erp-vo/src/main/java/com/capgemini/cn/erp/vo/RevenueShareRuleVo.java
+++ erp-vo/src/main/java/com/capgemini/cn/erp/vo/RevenueShareRuleVo.java
@@ -2,6 +2,7 @@ package com.capgemini.cn.erp.vo;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.sql.Timestamp;
